825ca5f7b47aa71f575754981bd954b26cf990a3
Miguelro123 <969@holbertonschool.com>
2019-09-11 03:07:56 +0000

    Print the ASCII alphabet except q and e

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for lowercase_letter in range(97, 123):
-    if ch != 101 and ch != 113:
+    if lowercase_letter != 101 and lowercase_letter != 113:
         print("{:c}".format(lowercase_letter),  end="")
