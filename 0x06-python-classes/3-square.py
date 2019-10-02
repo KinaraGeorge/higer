e29ed9bae24e95809dde1620e0497434da2386c5
Miguelro123 <969@holbertonschool.com>
2019-10-02 02:38:12 +0000

    Class Square that defines a square by: (based on 2-square.py)

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+class Square:
+    """empty class Square that defines a square"""
+    def __init__(self, size=0):
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+    def area(self):
+        '''Defines the area of a square'''
+        return self.__size * self.__size
