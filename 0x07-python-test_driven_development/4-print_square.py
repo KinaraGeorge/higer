5f9dd68d6171106ecfeb7cde2a6fc65960a2c6bf
Miguelro123 <969@holbertonschool.com>
2019-10-05 05:47:37 +0000

    Prints a square with the character #

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -6,7 +6,7 @@ A function to print a square
 """
 
 def print_square(size):
-
+    """Print a square made of #"""
     if type(size) is not int:
         raise TypeError("size must be an integer")
     if size < 0:
