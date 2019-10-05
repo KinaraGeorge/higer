84e2dfb6101a367496cd750bfc9936f8b543c10a
Miguelro123 <969@holbertonschool.com>
2019-10-05 05:42:34 +0000

    Prints My name is

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
 """
-
-Prints two strings as a name
-
+This module contains the say_my_name function.
+It accepts two strings with the second string given a default string.
+It will print out a string with the two input strings
+Checks whether or not the inputs are strings.
 """
-def say_my_name(first_name, last_name=""):
 
-    if type(first_name) is not str:
-        raise TypeError('first_name must be a string')
-    if type(last_name) is not str:
-        raise TypeError('last_name must be a string')
-    print("My name is {:s} {:s}".format(first_name, last_name))
+
+def say_my_name(first_name, last_name=""):
+        """
+        say_my_name: Takes up to two input strings and printsout a message
+        """
+        if not isinstance(first_name, str):
+                raise TypeError("first_name must be a string")
+        if not isinstance(last_name, str):
+                raise TypeError("last_name must be a string")
+        print("My name is {:s} {:s}".format(first_name, last_name))
