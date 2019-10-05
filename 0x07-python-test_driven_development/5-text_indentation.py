5bbb51f1064d18c9d91ffd649d1657641a6b88f1
Miguelro123 <969@holbertonschool.com>
2019-10-05 06:11:55 +0000

    Multiplies 2 matrices

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
+
+
+'''
+   5-text_indentation
+   Contains a function that adds new line after characters
+'''
+
+
 def text_indentation(text):
-
-    new_list = []
-    other_lista = []
-
-    if type(text) is not str:
-        raise TypeError("text must be a string")
-
-    str1 = text.replace('.', '.\n\n')
-    str2 = str1.replace('?', '?\n\n')
-    str1 = str2.replace(':', ':\n\n')
-    str1 = str1.strip()
-    new_list = str1.split("\n")
-
-    for i in new_list:
-        other_lista.append(i.strip())
-
-    print('\n'.join(other_lista))
+    '''Python function to add two new lines to string at character points'''
+    if not isinstance(text, str):
+        raise TypeError('text must be a string')
+    period = text.replace('.', '.\n\n')
+    period = period.replace('?', '?\n\n')
+    period = period.replace(':', ':\n\n')
+    period1 = period.split('\n')
+    for line in range(len(period1)):
+        print("{}".format(period1[line].strip()),
+              end=("" if (line == (len(period1) - 1)) else '\n'))
