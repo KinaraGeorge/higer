827f249b9998cc6f424dc41dc567da03b40cccf8
Miguelro123 <969@holbertonschool.com>
2019-10-08 02:54:37 +0000

    Prints My name is

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -3,9 +3,8 @@
 
 
 def say_my_name(first_name, last_name=""):
-        """say_my_name: Takes up to two input strings and printsout a message"""
         if not isinstance(first_name, str):
                 raise TypeError("first_name must be a string")
         if not isinstance(last_name, str):
                 raise TypeError("last_name must be a string")
-        print("My name is {:s} {:s}".format(first_name, last_name))
+        print("My name is {} {}".format(first_name, last_name))
