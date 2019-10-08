6b99b096ccd77ff7dde9f191d9654b10588f58bb
Miguelro123 <969@holbertonschool.com>
2019-10-08 02:46:29 +0000

    Prints My name is

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
-"""
-This module contains the say_my_name function.
-It accepts two strings with the second string given a default string.
-It will print out a string with the two input strings
-Checks whether or not the inputs are strings.
-"""
+"""This module contains the say_my_name function"""
 
 
 def say_my_name(first_name, last_name=""):
-        """
-        say_my_name: Takes up to two input strings and printsout a message
-        """
+        """say_my_name: Takes up to two input strings and printsout a message"""
         if not isinstance(first_name, str):
                 raise TypeError("first_name must be a string")
         if not isinstance(last_name, str):
