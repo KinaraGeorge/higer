bc4ab753157e934c94b8cb3d6e649bc3d491a8b2
Miguelro123 <969@holbertonschool.com>
2019-10-08 03:24:15 +0000

    Multiplies 2 matrices

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,49 +1,47 @@
 #!/usr/bin/python3
-"""Module that contains a function to divide a matrix by a scalar"""
+"""Matrix multiply module"""
 
 
 def matrix_mul(m_a, m_b):
-    if type(m_a) is not list:
-        raise TypeError("m_a must be a list")
-    if type(m_b) is not list:
-        raise TypeError("m_b must be a list")
-    for x in m_a:
-        if type(x) is not list:
-            raise TypeError("m_a must be a list of lists")
-    for x in m_b:
-        if type(x) is not list:
-            raise TypeError("m_b must be a list of lists")
-    if len(m_a) < 1:
+    if type(m_a) != list:
+        raise TypeError('m_a must be a list')
+
+    if type(m_b) != list:
+        raise TypeError('m_b must be a list')
+
+    if any(type(i) != list for i in m_a):
+        raise TypeError('m_a must be a list of lists')
+
+    if any(type(i) != list for i in m_b):
+        raise TypeError('m_b must be a list of lists')
+
+    if any(not i for i in m_a):
         raise ValueError("m_a can't be empty")
-    if len(m_b) < 1:
+
+    if any(not i for i in m_b):
         raise ValueError("m_b can't be empty")
-    arowlen = len(m_a[0])
-    if arowlen < 1:
-        raise ValueError("m_a can't be empty")
-    browlen = len(m_b[0])
-    if browlen < 1:
-        raise ValueError("m_b can't be empty")
-    for row in m_a:
-        if len(row) != arowlen:
-            raise TypeError("each row of m_a must should be of the same size")
-        for col in row:
-            if type(col) is not float and type(col) is not int:
-                raise TypeError("m_a should contain only integers or floats")
-    for row in m_b:
-        if len(row) != browlen:
-            raise TypeError("each row of m_b must should be of the same size")
-        for col in row:
-            if type(col) is not float and type(col) is not int:
-                raise TypeError("m_b should contain only integers or floats")
-    if arowlen != len(m_b):
+
+    if any(any(type(j) != int and type(j) != float for j in i) for i in m_a):
+        raise TypeError('m_a should contain only integers or floats')
+
+    if any(any(type(j) != int and type(j) != float for j in i) for i in m_b):
+        raise TypeError('m_b should contain only integers or floats')
+
+    l_a = len(m_a[0])
+    if any(len(row) != l_a for row in m_a):
+        raise TypeError('each row of m_a must should be of the same size')
+
+    l_b = len(m_b[0])
+    if any(len(row) != l_b for row in m_b):
+        raise TypeError('each row of m_b must should be of the same size')
+
+    if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
-    newmatrix = []
-    for x in range(len(m_a)):
-        newrow = []
-        for y in range(browlen):
-            sum = 0
-            for z in range(arowlen):
-                sum += m_a[x][z] * m_b[z][y]
-            newrow.append(sum)
-        newmatrix.append(newrow)
-    return newmatrix
+
+    result = [[0 for row in range(len(m_b[0]))] for col in range(len(m_a))]
+
+    for i in range(len(m_a)):
+        for j in range(len(m_b[0])):
+            for k in range(len(m_b)):
+                result[i][j] += m_a[i][k] * m_b[k][j]
+    return result
