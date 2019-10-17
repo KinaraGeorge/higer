4a01557a56d3d3fb1e9939b75a4abe5776b9864f
Miguelro123 <969@holbertonschool.com>
2019-10-17 03:39:01 +0000

    Multiplies 2 matrices

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -2,8 +2,8 @@
 
 from sys import argv
 import os.path
-save = __import__('7-save_to_json_file').save
-load = __import__('8-load_from_json_file').load
+save = __import__('7-save_to_json_file').save_to_json_file
+load = __import__('8-load_from_json_file').load_from_json_file
 
 
 filename = 'add_item.json'
