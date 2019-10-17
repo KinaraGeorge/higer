4b0254d63148341498f6da309f4a92f648de61de
Miguelro123 <969@holbertonschool.com>
2019-10-17 04:11:27 +0000

    Reads stdin line by line and computes metrics

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
+import sys
 
 
-class Student:
-
-    def __init__(self, first, last, age):
-        self.first_name = first
-        self.last_name = last
-        self.age = age
-
-    def to_json(self):
-
-        retdict = {}
-        objdict = self.__dict__
-        for ele in objdict:
-            if type(objdict[ele]) in [list, dict, str, int, bool]:
-                retdict[ele] = objdict[ele]
-        return retdict
+codes = {}
+totalsize = 0
+linesread = 0
+try:
+    for line in sys.stdin:
+        line = line.split(" ")
+        if line[-2] in codes:
+            codes[line[-2]] += 1
+        else:
+            codes[line[-2]] = 1
+        totalsize += int(line[-1])
+        linesread += 1
+        if linesread % 10 == 0:
+            print("File size:", totalsize)
+            for code in sorted(list(codes)):
+                print("{}: {}".format(code, codes[code]))
+finally:
+    print("File size:", totalsize)
+    for code in sorted(list(codes)):
+        print("{}: {}".format(code, codes[code]))
