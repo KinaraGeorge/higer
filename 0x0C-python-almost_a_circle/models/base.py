3c26170731cabb11b503de929d77e48f4552e91e
Miguelro123 <969@holbertonschool.com>
2019-10-22 20:28:14 +0000

    First class Base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python3
+"""
+This module contains the class Base
+"""
 import json
+import csv
+import turtle
+import random
 
 
-class Base():
-
+class Base:
+    """
+    This class contains the private attribute nb objects and a constructor
+    """
     __nb_objects = 0
 
     def __init__(self, id=None):
-
+        """
+        constructs id based on input
+        """
         if id is not None:
             self.id = id
         else:
@@ -15,6 +25,146 @@ class Base():
             self.id = Base.__nb_objects
 
     @staticmethod
-    def reset():
+    def to_json_string(list_dictionaries):
+        """
+        Returns an input dictionary as JSON
+        """
+        if list_dictionaries is None:
+            return "[]"
+        return json.dumps(list_dictionaries)
 
-        Base._Base__nb_objects = 0
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        Reverts from json string
+        """
+        if json_string is None:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        Saves a JSON string to a file
+        """
+        strings = []
+        if list_objs is not None:
+            for i in list_objs:
+                strings.append(cls.to_dictionary(i))
+        with open(cls.__name__ + ".json", 'w') as open_file:
+            open_file.write(cls.to_json_string(strings))
+
+    @classmethod
+    def create(cls, **dictionary):
+        """
+        Creates a new instance of cls and returns updated values
+        """
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        if cls.__name__ == "Square":
+            dummy = cls(1)
+        dummy.update(**dictionary)
+
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """
+        Load information from a file and create proper class instances
+        """
+        lists = []
+        try:
+            with open(cls.__name__ + ".json", "r", encoding="utf-8") as opened:
+                lists = cls.from_json_string(opened.read())
+            for i in range(len(lists)):
+                lists[i] = cls.create(**lists[i])
+            return lists
+        except Exception:
+            return lists
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """
+        Save to a csv format
+        """
+        if cls.__name__ == "Rectangle":
+            fields = ["id", "width", "height", "x", "y"]
+        if cls.__name__ == "Square":
+            fields = ["id", "size", "x", "y"]
+
+        with open("{}.csv".format(cls.__name__), 'w')as opened:
+
+            if list_objs is None:
+                le_writer = csv.writer(opened)
+                le_writer.writerow([[]])
+
+            else:
+                le_writer = csv.DictWriter(opened, fieldnames=fields)
+                le_writer.writeheader()
+                for i in list_objs:
+                    le_writer.writerow(i.to_dictionary())
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """
+        Load data from a csv file
+        """
+        lists = []
+        try:
+            with open("{}.csv".format(cls.__name__), newline='') as opened:
+                dict_reader = csv.DictReader(opened)
+                for i in dict_reader:
+                    for key, value in i.items():
+                        i[key] = int(value)
+                    dummy = cls.create(**i)
+                    lists.append(dummy)
+                return lists
+        except Exception:
+            return lists
+
+    @staticmethod
+    def draw(list_rectangles, list_squares):
+        """
+        Draw shapes in rainbow
+        """
+        turtle.colormode(255)
+
+        def color(x, y):
+            R = random.randint(0, 256)
+            G = random.randint(0, 256)
+            B = random.randint(0, 256)
+            turtle.pencolor(R, G, B)
+            turtle.left(90)
+        for i in list_rectangles:
+            cnt = 0
+            x = i.x
+            y = i.y
+            width = i.width
+            height = i.height
+
+            turtle.setpos(x, y)
+            turtle.pendown()
+            while cnt < 5:
+                if cnt % 2 == 0:
+                    turtle.fd(height)
+                else:
+                    turtle.fd(width)
+                color(x, y)
+                cnt += 1
+            turtle.penup()
+
+        for i in list_squares:
+            cnt = 0
+            x = i.x
+            y = i.y
+            size = i.size
+
+            turtle.setpos(x, y)
+            turtle.pendown()
+            while cnt < 5:
+                turtle.fd(size)
+                color(x, y)
+                cnt += 1
+            turtle.penup()
+
+        turtle.exitonclick()
--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,4 +1,5 @@
-k!/usr/bin/python3
+#!/usr/bin/python3
+
 
 import unittest
 from models.base import Base
@@ -6,82 +7,39 @@ from models.base import Base
 
 class TestBaseClass(unittest.TestCase):
 
+    def test_doctest(self):
 
+        self.assertIsNotNone(Base.__doc__)
 
-    def test_id_valid(self):
+    def test_base_class(self):
 
-        b = Base()
-        self.assertEqual(b.id, 1)
+        test = Base()
+        self.assertTrue(issubclass(type(test), Base))
 
-    def test_id_valid_multiple(self):
+    def test_base_combined(self):
+
+        initial = Base()
+        one = initial.id
+        initial_one = Base(75)
+        two = Base()
+        self.assertTrue(one + 1, two.id)
+
+    def test_base_empty_id(self):
 
         b1 = Base()
-        self.assertEqual(b1.id, 2)
+        self.assertEqual(b1.id, 4)
         b2 = Base()
-        self.assertEqual(b2.id, 3)
+        self.assertEqual(b2.id, 5)
         b3 = Base()
-        self.assertEqual(b3.id, 4)
+        self.assertEqual(b3.id, 6)
+
+    def test_base_normal(self):
+
         b4 = Base(12)
         self.assertEqual(b4.id, 12)
-        b5 = Base()
-        self.assertEqual(b5.id, 5)
+        b5 = Base(2)
+        self.assertEqual(b5.id, 2)
 
-    def test_id_valid_same_id(self):
-
-        b6 = Base(77)
-        self.assertEqual(b6.id, 77)
-        b7 = Base(77)
-        self.assertEqual(b7.id, 77)
-
-    def test_id_valid_param_str(self):
-
-        b8 = Base("Holberton")
-        self.assertEqual(b8.id, "Holberton")
-
-    def test_id_valid_param_list(self):
-
-        b9 = Base([98, 977])
-        self.assertEqual(b9.id, [98, 977])
-
-    def test_id_valid_param_tuple(self):
-
-        b10 = Base((98, 977))
-        self.assertEqual(b10.id, (98, 977))
-
-    def test_id_valid_param_dict(self):
-
-        b11 = Base({"98": 977})
-        self.assertEqual(b11.id, {"98": 977})
-
-    def test_id_valid_param_set(self):
-
-        b12 = Base({"98", 977})
-        self.assertEqual(b12.id, {"98", 977})
-
-    def test_id_valid_param_float(self):
-
-        b13 = Base(3.14)
-        self.assertEqual(b13.id, 3.14)
-
-    def test_id_valid_param_inf(self):
-
-        b14 = Base(float('inf'))
-        self.assertEqual(b14.id, float('inf'))
-
-    def test_id_valid_param_func(self):
-
-        b15 = Base(len)
-        self.assertEqual(b15.id("Hi"), 2)
-
-    def test_id_valid_isprivate(self):
-
-        with self.assertRaises(AttributeError):
-            print(Base.__nb_objects)
-
-    def test_params_exceptions_args_six(self):
-
-        with self.assertRaises(TypeError):
-            r = Base(2, 3, 23, 12, 234, 4)
 
 if __name__ == '__main__':
     unittest.main()
