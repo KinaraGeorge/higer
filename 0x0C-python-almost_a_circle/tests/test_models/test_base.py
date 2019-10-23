12dc1cb9009718b51c4d869bf9536270c4a8d651
Miguelro123 <969@holbertonschool.com>
2019-10-23 00:41:26 +0000

    Test

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-""" Unit tests for models/base.py """
 
 from models.base import Base
 import unittest
@@ -8,7 +7,7 @@ import unittest
 class Test_Base(unittest.TestCase):
 
     def test_simple(self):
-        """ simplest test cases """
+
         x = Base(1)
         self.assertEqual(1, x.id)
         self.assertNotEqual(99, x.id)
@@ -16,12 +15,12 @@ class Test_Base(unittest.TestCase):
         self.assertEqual(x.id, 99)
 
     def test_InputErrors(self):
-        """ tests for input error """
+
         with self.assertRaises(AttributeError):
             self.assertIsEqual(x(), 1)
 
     def test_empty(self):
-        """ test empty conditions """
+
         x = Base()
         self.assertEqual(1, x.id)
         x = Base(None)
@@ -32,14 +31,14 @@ class Test_Base(unittest.TestCase):
         self.assertEqual(7, x.id)
 
     def test_string(self):
-        """ tests strings """
+
         x = Base(2)
         self.assertEqual(2, x.id)
         x = Base("2")
         self.assertEqual('2', x.id)
 
     def test_float(self):
-        """ test floats """
+
         x = Base(3)
         self.assertEqual(3, x.id)
         x = Base(3.45)
@@ -48,7 +47,7 @@ class Test_Base(unittest.TestCase):
         self.assertEqual(-4.56, x.id)
 
     def test_weird(self):
-        """ test wtf things """
+
         x = Base(4)
         self.assertEqual(4, x.id)
         x = Base([1, 2])
@@ -67,7 +66,7 @@ class Test_Base(unittest.TestCase):
         self.assertEqual(6, x.id)
 
     def test_more_errors(self):
-        """ more error checking """
+
         x = Base(float('inf'))
         self.assertEqual(float('inf'), x.id)
         x = Base(float('NaN'))
@@ -96,7 +95,7 @@ class Test_Base(unittest.TestCase):
         self.assertEqual(5, x.id)
 
         def test_id(self):
-            """ tests the if id works properly """
+
             goku = Base()
             gokuID = goku.id
             goku2 = Base(69)
@@ -110,7 +109,7 @@ class Test_Base(unittest.TestCase):
             seld.assertEqual(goku3.id, 100)
 
         def test_class(self):
-            """ tests subclass """
+
             goku = Base()
             self.assertTrue(issubclass(type(goku), Base))
 
