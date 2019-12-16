f1996b9f329ae090318608f130c7ae9e04a5aae9
Miguelro123 <969@holbertonschool.com>
2019-12-16 02:36:57 +0000

    Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,17 +2,15 @@
 """ Python scripte to list items from MySQL"""
 
 import MySQLdb
-
 from sys import argv
-
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3], charset="utf8")
-    c = db.cursor()
-    c.execute('SELECT * FROM states ORDER BY id ASC')
-    for rows in c.fetchall():
-        print(rows)
-
-    c.close()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
     db.close()
