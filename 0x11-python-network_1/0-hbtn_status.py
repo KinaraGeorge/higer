364a0515bc58d1ce5615909c5ef3640f4681bac9
Miguelro123 <969@holbertonschool.com>
2020-01-13 20:36:14 +0000

    script that fetches a html

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 # Python script that fetches a url
+
 import urllib.request as web
 
 if __name__ == "__main__":
 
     with web.urlopen('https://intranet.hbtn.io/status') as URL:
-
         html = URL.read()
-
-    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(html), html))
-    print('\t- utf8 content: {}'.format(html.decode('utf-8')))
+    print("Body response:")
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(html.decode('utf-8')))
