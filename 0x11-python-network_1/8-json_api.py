aa60c7aaf05f668f80b61d4bdc2130e704a24ddb
Miguelro123 <969@holbertonschool.com>
2020-01-13 23:19:07 +0000

    script that takes in a letter and sends a POST

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,18 +6,17 @@ from sys import argv
 
 if __name__ == "__main__":
 
-    try:
+    letter = ""
+    if len(argv) == 2:
         letter = argv[1]
-    except BaseException:
-        letter = ""
+
     try:
         r = requests.post('http://0.0.0.0:5000/search_user', data={'q': letter})
-        r = r.json()
-        r_id = r.get('id')
-        r_name = r.get('name')
-        if r_id is None or r_name is None:
-            print('No result')
+        j = r.json()
+
+        if j:
+            print('[{}] {}'.format(j.get('id'), j.get('name')))
         else:
-            print('[{}] {}'.format(r_id, r_name))
-    except BaseException:
+            print('No result')
+    except ValueError:
         print('Not a valid JSON')
