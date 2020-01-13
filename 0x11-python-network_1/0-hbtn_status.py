1df53494095eaeaf5d5f35a0074620e394306a23
Miguelro123 <969@holbertonschool.com>
2020-01-13 20:45:22 +0000

    script that fetches a html

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,8 +9,6 @@ if __name__ == "__main__":
 
         html = URL.read()
 
-    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(html), html))
-    print('\t- utf8 content: {}'.format(html.decode('utf-8')))
     print("Body response:")
     print("\t- type: {}".format(type(html)))
     print("\t- content: {}".format(html))
