3df92eecd8d356f0ff6c1235db085fb9634e7286
Miguelro123 <969@holbertonschool.com>
2020-01-13 22:15:31 +0000

    script that takes in a URL and an email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,12 +5,8 @@
 import urllib.request
 import urllib.parse
 import sys
-
 if __name__ == "__main__":
-    post_url = sys.argv[1]
-    post_data = urllib.parse.urlencode({'email': sys.argv[2]}).encode('utf8')
-
-    with urllib.request.urlopen(url=post_url, data=post_data) as response:
-        posted = response.read()
-
-    print(posted.decode('utf8'))
+    a = sys.argv[1]
+    b = parse.urlencode({"email": sys.argv[2]}).encode('utf8')
+    with request.urlopen(a, b) as response:
+        print(response.read().decode('utf-8')
