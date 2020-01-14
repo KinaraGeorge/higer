37a703cc6e9a4ff14f03f8ee7f872853c0e39cf8
Miguelro123 <969@holbertonschool.com>
2020-01-14 00:36:11 +0000

    script that takes your Github credentials

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+script that uses github autho
+"""
+
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    r = requests.get('https://api.github.com/user',
+                     auth=(argv[1], argv[2]))
+    if r.status_code == 200:
+        j = r.json()
+        print(j.get('id'))
+    else:
+        print('None')
