62ec4f58cb6cb1675aec82a441d6bb0f78adcaaf
Miguelro123 <969@holbertonschool.com>
2020-02-05 03:03:37 +0000

    script that prints x times C is fun

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 let arg = process.argv[2];
-let count = 0;
 if (isNaN(arg)) {
   console.log('Missing number of occurrences');
-}
-while (count < arg) {
-  console.log('C is fun');
-  count++;
+} else {
+    while (count < arg) {
+	console.log('C is fun');
+	count++;
+    }
 }
