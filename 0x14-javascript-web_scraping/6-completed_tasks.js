0d3ec2d8db5b8f4e12b255f2bc1d68071988b028
Miguelro123 <969@holbertonschool.com>
2020-02-11 19:50:51 +0000

    computes the number of tasks completed by user id

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,20 +1,24 @@
 #!/usr/bin/node
 const request = require('request');
-const final = {};
-const indiv;
-request.get(process.argv[2], function (error, request, body) {
-  if (error) {
-    console.log(error);
-  }
-  if (request.statusCode === 200) {
-    for (indiv of JSON.parse(body)) {
-      if (indiv.completed === true) {
-        if (final[indiv.userId] === undefined) {
-          final[indiv.userId] = 0;
+const url = process.argv[2];
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    let completed = {};
+    let tasks = JSON.parse(body);
+    for (let i in tasks) {
+      let task = tasks[i];
+      if (task.completed === true) {
+        if (completed[task.userId] === undefined) {
+          completed[task.userId] = 1;
+        } else {
+          completed[task.userId]++;
         }
-        final[indiv.userId]++;
       }
     }
+    console.log(completed);
+  } else {
+    console.log('An error occured. Status code: ' + response.statusCode);
   }
-  console.log(final);
 });
