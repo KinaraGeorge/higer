f5342e98cbb9103dc2bc43a6758d5ac88d9b3fe9
Miguelro123 <969@holbertonschool.com>
2020-02-11 18:29:34 +0000

    prints the title of a Star Wars movie

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-let request = require('request');
+const request = require('request');
 request('http://swapi.co/api/films/' + process.argv[2], function (error, response, body) {
   if (error) {
     console.log(error);
