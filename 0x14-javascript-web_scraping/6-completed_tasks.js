184495d6c808d1949958df42d6989d1a226ac077
Miguelro123 <969@holbertonschool.com>
2020-02-11 19:40:27 +0000

    computes the number of tasks completed by user id

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const request = require('request');
+let final = {};
+const indiv;
+request.get(process.argv[2], function (error, request, body) {
+  if (error) {
+    console.log(error);
+  }
+  if (request.statusCode === 200) {
+    for (indiv of JSON.parse(body)) {
+      if (indiv.completed === true) {
+        if (final[indiv.userId] === undefined) {
+          final[indiv.userId] = 0;
+        }
+        final[indiv.userId]++;
+      }
+    }
+  }
+  console.log(final);
+});
